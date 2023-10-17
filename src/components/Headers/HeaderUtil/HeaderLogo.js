3c4b58ff1c51f1000242e4e84c597a35ebc28d8a
iliask796 <iliaskal7@gmail.com>
2023-10-17 13:20:31 +0300

    Added title to HeaderLogo

--- src/components/Headers/HeaderUtil/HeaderLogo.js
+++ src/components/Headers/HeaderUtil/HeaderLogo.js
@@ -2,7 +2,7 @@ import { Typography} from '@mui/material';
 
 const HeaderLogo = () => {
   return (
-    <div className='logo'>
+    <div title='Clamsy is a sample full-stack project developed by Ilias Kalamatas' className='logo'>
         <Typography variant='h4' className='logo-text'>Clamsy</Typography>
         <img className='logo-img'
           src='clumsy.png'
