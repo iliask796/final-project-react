34b1956f984dfc8aa1c1374fbe1a03fa6dd0fe82
iliask796 <iliaskal7@gmail.com>
2023-10-18 18:04:49 +0300

    Added Popoever to edit task

--- src/components/Main/MainUtil/Task.js
+++ src/components/Main/MainUtil/Task.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import {useState} from 'react'
 
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import Popover from '@mui/material/Popover';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import Fab from '@mui/material/Fab';
 import IconButton from '@mui/material/IconButton';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
@@ -9,33 +13,75 @@ import SpeakerNotesIcon from '@mui/icons-material/SpeakerNotes';
 import SpeakerNotesOffIcon from '@mui/icons-material/SpeakerNotesOff';
 
 const Task = (props) => {
-    const {id, title, description} = props.item
+    const {id, title, description} = props.item;
+
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleDelete = () => {
+        handleClose();
+    }
+
+    const open = Boolean(anchorEl);
+    const popId = open ? 'simple-popover' : undefined;
 
     return (
-        <ListItem 
-            key={id}
-            className='list-item'
-            secondaryAction={
-                <>
-                    <IconButton edge="end" aria-label="shift-left">
-                        <NavigateBeforeIcon className='this-button'/>
-                    </IconButton>
-                    <IconButton edge="end" aria-label="shift-right">
-                        <NavigateNextIcon />
-                    </IconButton>
-                </>
-            }
-        >
-            <ListItemText
-                primary={title}
-                secondary= {
-                    description ?
-                    <SpeakerNotesIcon fontSize='small' /> :
-                    <SpeakerNotesOffIcon fontSize='small'/>
-                    }
-                className='list-item-text'
-            />
-        </ListItem>
+        <>
+            <ListItem 
+                key={id}
+                className='list-item'
+                secondaryAction={
+                    <>
+                        <IconButton edge="end" aria-label="shift-left">
+                            <NavigateBeforeIcon className='this-button'/>
+                        </IconButton>
+                        <IconButton edge="end" aria-label="shift-right">
+                            <NavigateNextIcon />
+                        </IconButton>
+                    </>
+                }
+            >
+                <ListItemText
+                    primary={title}
+                    secondary= {
+                        description ?
+                        <SpeakerNotesIcon fontSize='small' /> :
+                        <SpeakerNotesOffIcon fontSize='small'/>
+                        }
+                    className='list-item-text'
+                    onClick={handleClick}
+                />
+            </ListItem>
+            <Popover
+                id={popId}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
+                }}
+                transformOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+            >
+                <Paper elevation={3} sx={{p: 1}}>
+                    <TextField id="outlined-basic-1" label="Title" defaultValue={title} variant="outlined" sx={{m:0.5}}/>
+                    <TextField id="outlined-basic-2" label="Description" defaultValue={description} variant="outlined" sx={{m:0.5}}/>
+                    <Fab onClick={handleDelete} color='success' variant='extended' aria-label="delete" sx={{m:0.5, backgroundColor: "#00A36C"}}>
+                        Delete
+                    </Fab>
+                </Paper>
+            </Popover>
+        </>
     )
 }
 
