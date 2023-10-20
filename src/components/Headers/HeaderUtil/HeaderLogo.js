eb5c81c9a3942763c66faab35f403b98df643452
iliask796 <iliaskal7@gmail.com>
2023-10-20 16:27:06 +0300

    Added logo to utility folder

--- src/components/Headers/HeaderUtil/HeaderLogo.js
+++ src/components/Headers/HeaderUtil/HeaderLogo.js
@@ -1,11 +1,12 @@
 import { Typography} from '@mui/material';
+import Logo from '../../../utility/images/clumsy.png'
 
 const HeaderLogo = () => {
   return (
     <div title='Clamsy is a sample full-stack project developed by Ilias Kalamatas' className='logo'>
         <Typography variant='h4' className='logo-text'>Clamsy</Typography>
         <img className='logo-img'
-          src='clumsy.png'
+          src={Logo}
           alt='clumsy-logo'
         />
       </div>
